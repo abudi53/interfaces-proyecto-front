73d41c67e8ac90a7722a4d5dc66442744bdabe7a
abudi53 <abdallahffh@gmail.com>
2023-12-10 20:23:23 -0400

    Agregar admin, auth, falta register y agregar libro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './auth.guard';
+import { AdminComponent } from './admin/admin.component';
 
 export const APP_ROUTES: Routes = [
     { path: 'registrarse', component: RegisterComponent },
     { path: 'iniciar-sesion', component: LoginComponent },
+    { path: 'admin', component: AdminComponent, canActivate: [AuthGuard]},
     { path: '', redirectTo: '/iniciar-sesion', pathMatch: 'full' }
 ];
 
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,14 +1,48 @@
 import { Component } from '@angular/core';
-
-import { User } from '../user';
+import { HttpClient, HttpBackend} from '@angular/common/http';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
+
+
 export class LoginComponent {
 
+  private http: HttpClient;
+
+  constructor(private router: Router, handler: HttpBackend) { 
+    this.http = new HttpClient(handler);
+  }
+
+  readonly API_LOGIN: string = '/api/auth/login';
+
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  onSubmit(event: Event) {
+    event.preventDefault(); 
+    const data = this.loginForm.value;    
+
+    this.http.post(this.API_LOGIN, data, { withCredentials: true }).subscribe(
+      (response: any) => {
+        localStorage.setItem('authToken', response.access_token);
+
+        this.router.navigate(['/admin']);
+
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+
+    
+}
 }
